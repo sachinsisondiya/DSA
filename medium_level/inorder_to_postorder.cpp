62d7a30c4a9da7b03b2d9a8604a084c3c30d31a0
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-05-26 23:31:54 +0530

    add how to construct tree

---
+++ medium_level/inorder_to_postorder.cpp
@@ -0,0 +1,95 @@
+//
+// Created by sachi on 5/26/2025.
+//
+#include<iostream>
+using namespace std;
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+    node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+//node* built(){
+//    int data;
+//    cin>>data;
+//
+//    if(data==-1){
+//        return NULL;
+//    }
+//
+//    node* root=new node(data);
+//
+//    root->left=built();
+//    root->right=built();
+//
+//    return root;
+//}
+
+int find(int arr1[],int start,int end,int element){
+
+    for(int i=start;i<=end;i++){
+        if(element==arr1[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+node* postorder(int arr1[],int arr2[],int &poststart,int instart,int inend,int n){
+    // base case
+    if(poststart <0 || instart> inend){
+        return NULL;
+    }
+    int element= arr2[poststart--];
+    node* root=new node(element);
+
+    int index=find(arr1,instart,inend,element);
+    root->right=postorder(arr1,arr2,poststart,index+1,inend,n);
+    root->left=postorder(arr1,arr2,poststart,instart,index-1,n);
+
+
+    return root;
+
+}
+void printf(node *root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    printf(root->left);
+    printf(root->right);
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr1[10];
+    int arr2[10];
+
+    for(int i=0;i<n;i++){
+        cout<<"enter the inorder element";
+        cin>>arr1[i];
+    }
+    cout<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<"enter the preorder element";
+        cin>>arr2[i];
+    }
+    cout<<endl;
+    int poststart=n-1;
+
+
+    node* root=postorder(arr1,arr2,poststart,0,n-1,n);
+    printf(root);
+
+
+
+
+
+
+}
