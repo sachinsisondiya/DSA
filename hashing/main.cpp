43f593959e39dc8dbbb7eeb99985da65d6be547a
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-07-14 22:06:08 +0530

    hashing

---
+++ hashing/main.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//    int n;
+//    cin>>n;
+//    int arr[n];
+//    for(int i=0;i<n;i++){
+//        cin>>arr[i];
+//    }
+//    int freq[n];
+//    for(int i=0;i<n;i++){
+//        freq[i]=0;
+//    }
+//    for(int i=0;i<n;i++){
+//        freq[arr[i]]++;
+//        cout<<i;
+//    }
+//
+////    int test;
+////    cin>>test;
+////
+////    while(test--){
+////        int m;
+////        cin>>m;
+////
+////        cout<<freq[m];
+////    }
+////    return 0;
+//}
