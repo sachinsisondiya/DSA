fcd41b8f839a3bb20e541e66adaecb6af034f6b9
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-07-25 12:18:52 +0530

    medium level

---
+++ sorting/longest_consecutive_sequence.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//int main(){
+//    int n;
+//    cin>>n;
+//    int arr[n];
+//    for(int i=0;i<n;i++){
+//        cin>>arr[i];
+//    }
+//    sort(arr ,arr+n);
+////    for(int i=0;i<n;i++){
+////        cout<<arr[i];
+////    }
+//
+//    int maxi=-1;
+//    int count=1;
+//    for(int i=0;i<n-1;i++){
+//
+//        if(arr[i]+1 == arr[i+1]){
+//            count++;
+//            maxi=max(maxi,count);
+//        }
+//        else{
+//            count=1;
+//        }
+//    }
+//    cout<<maxi;
+//}
