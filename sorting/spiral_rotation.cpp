efd28ab2b5c0350e1aa22897fdc0e474f37860c9
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-07-26 13:22:43 +0530

    medium level

---
+++ sorting/spiral_rotation.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>>a(n,vector<int>(n,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    int left=0;
+    int right= n-1;
+    int top=0;
+    int bottom=n-1;
+    vector<int>ans;
+    while(top<=bottom && left<=right){
+
+        for(int i=left;i<=right;i++){
+            ans.push_back(a[top][i]);
+        }
+        top++;
+
+        for(int i=top;i<bottom;i++){
+            ans.push_back(a[i][right]);
+        }
+        right--;
+
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                ans.push_back(a[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                ans.push_back(a[i][left]);
+            }
+            left++;
+        }
+
+    }
+    for(auto it:ans){
+        cout<<it<<" ";
+    }
+}
