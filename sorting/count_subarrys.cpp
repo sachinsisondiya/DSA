30f78d7c59023c9379ba51351b5b7663ab58fa54
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-07-27 12:05:24 +0530

    medium level

---
+++ sorting/count_subarrys.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//#include<map>
+//using namespace std;
+//int main(){
+//    int n,k;
+//    cin>>n>>k;
+//
+//    int arr[n];
+//    for(int i=0;i<n;i++){
+//        cin>>arr[i];
+//    }
+//     map<int,int> m;
+//    m[0]++;
+//    int count=0;
+//    int sum =0;
+//    for(int i=0;i<n;i++){
+//        sum += arr[i];
+//        int rem = sum-k;
+//        count += m[rem];
+//        m[sum]++;
+//    }
+//    cout<<count;
+//
+////        for(int j=i+1;j<n;j++){
+////            sum +=arr[j];
+////
+////            if(sum == k){
+////                count++;
+////
+////            }
+////            else if(sum>k){
+////                break;
+////            }
+//
+//}
