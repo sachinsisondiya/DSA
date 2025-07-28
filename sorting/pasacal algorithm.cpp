05a9f7787969465c0dc3ec78445a3d082134837b
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-07-28 13:01:06 +0530

    medium level

---
+++ sorting/pasacal algorithm.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//void  func(int n, vector<int> &temp){
+//    int res =1;
+//    temp.push_back(res);
+//    for(int i=1;i<n;i++){
+//        res = res * (n-i);
+//        res = res/i;
+//        temp.push_back(res);
+//    }
+//
+//
+//}
+//int main(){
+//    int n;
+//    cin>>n;
+//    vector<vector<int>> ans;
+//
+//    for(int i=1;i<=n;i++){
+//        vector<int> temp;
+//        func(i,temp);
+//        ans.push_back(temp);
+//    }
+//    for(auto it:ans){
+//        for(auto j:it){
+//            cout<<j<<" ";
+//        }
+//        cout<<endl;
+//    }
+//
+//}
