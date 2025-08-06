03ea464a19e44e7f415f48e9a1bd78c739598f7e
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-08-06 12:31:17 +0530

    medium

---
+++ strings/subsequence_optimized.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//    string s;
+//    string g;
+//    getline(cin,s);
+//    getline(cin,g);
+//    int i =0;
+//    int  j =0;
+//    if(s.length() < g.length()){
+//        cout<<"not subsequense";
+//        return 0;
+//    }
+//    while(i<s.length()){
+//        if(s[i] == g[j]){
+//            i++;
+//            j++;
+//        }
+//        else{
+//            i++;
+//        }
+//    }
+//    if(j == g.length()){
+//
+//        cout<<"yes"<<" "<<j;
+//    }
+//    else{
+//        cout<<"no";
+//    }
+//}
