3c338e646d4bf20e714829287c7f5e4e494fb929
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-08-28 13:37:47 +0530

    easy

---
+++ recursion/print_all_the_possible_subsequences.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//using namespace std;
+//void sus(string s , string temp , int index){
+//    if(index >= s.length()){
+//        cout<<temp<<" ";
+//        return ;
+//    }
+//    temp.push_back(s[index]);
+//    sus(s , temp , index +1);
+//    temp.pop_back();
+//    sus(s,  temp , index +1);
+//
+//}
+//int main(){
+//    string s;
+//    getline(cin,s);
+//
+//    sus(s , "" , 0);
+//    return 0;
+//}
